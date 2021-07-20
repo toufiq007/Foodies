9d39ffe5d7a91653c070f637b2d57c090cb07eeb
toufiq007 <t.limon2342@gmail.com>
2021-07-21 00:50:48 +0600

    make counter section

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("scroll-on");
+  }else{
+    nav.classList.remove("scroll-on")
+  }
+}
+
+
+
+// counter design
+document.addEventListener("DOMContentLoaded",()=>{
+  function counter(id,start,end,duration){
+    let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(()=>{
+      current+=increment;
+      obj.textContent = current;
+      if(current==end){
+        clearInterval(timer);
+      }
+    },step)
+  }
+  counter("count-1",0,1232,3000);
+  counter("count-2",200,400,2000);
+  counter("count-3",0,1000,2400);
+  counter("count-4",0,1000,2400);
+})
